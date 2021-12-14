8ecb5b09c2aaa6486a28d6d627fd423d7b222849
anna-tsche <50755776+anna-tsche@users.noreply.github.com>
2021-12-14 19:59:46 +0300

    corrected task 1

--- Lesson 6/task 1.sql
+++ Lesson 6/task 1.sql
@@ -1,7 +1,7 @@
 -- original from lesson
-#SELECT media_type_id, (select name from media_types where id = media_type_id) as media_type, count(*)
-#FROM vk.media 
-#group by media_type_id;
+SELECT media_type_id, (select name from media_types where id = media_type_id) as media_type, count(*)
+FROM vk.media 
+group by media_type_id;
 
 -- updated without using join
 with medias_temp as (
