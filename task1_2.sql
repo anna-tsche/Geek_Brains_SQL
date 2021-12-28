599bbbcd47036a0b4d91554c53852ab98ec50d5f
anna-tsche <50755776+anna-tsche@users.noreply.github.com>
2021-12-28 19:11:28 +0300

    mandatory tasks added

---
+++ task1_2.sql
@@ -0,0 +1,10 @@
+-- Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
+
+SELECT * FROM products;
+SELECT * FROM catalogs;
+
+CREATE OR REPLACE VIEW product_category AS
+SELECT products.name AS product, catalogs.name AS category
+FROM products
+LEFT JOIN catalogs 
+ON products.catalog_id = catalogs.id;
